correct variance ratio

src/ipcas/ipcas.py
@@ -20,19 +20,19 @@ class IPCAS:
     def partial_fit(self, X:spmatrix):
         mco = MeanCenterOperator(X, mu=self.mean_, std=self.std_)
         if self.components_ is None:
-            _, s, Vt = svds(mco, self.n_components_)
+            _, s, Vt = svds(mco, self.n_components_, which="LM")
         else:
             _, R_s, R_Vt = svds(ResidualProjectionOperator(mco, self.components_.T),
-                                self.n_components_)
+                                self.n_components_, which="LM")
             _, s, Vt = svds(np.vstack((diags(R_s) @ R_Vt,
                                        diags(self.singular_values_) @ self.components_)),
-                            self.n_components_)
+                            self.n_components_, which="LM")
         idx = np.argsort(-s)
         self.n_samples_seen_ += X.shape[0]
         self.singular_values_ = s[idx]
         self.components_ = Vt[idx, :]
         self.explained_variance_ = self.singular_values_ ** 2 / (self.n_samples_seen_ - 1)
-        self.explained_variance_ratio_ = self.singular_values_ ** 2 / np.sum(self.std_ ** 2 * self.n_samples_seen_)
+        self.explained_variance_ratio_ = self.singular_values_ ** 2 / np.sum(np.ones_like(self.std_) ** 2 * self.n_samples_seen_)
     def transform(self, X:spmatrix):
         if self.components_ is None:
             raise ValueError("Error: Not fitted yet")